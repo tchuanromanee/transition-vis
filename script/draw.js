65d617e0956ff1f9f6dec96ba31a81319d1ffcd5
Tya Chuanromanee <tchuanro@nd.edu>
2022-11-01 15:33:20 -0400

    Finish initial body

--- script/draw.js
+++ script/draw.js
@@ -7,15 +7,17 @@ var svgContainer = d3.select("#main").append("svg")
 //Bezier Curve: P1, P2, Pfinal
 var path = d3.path();
 path.moveTo(300,120);
-path.bezierCurveTo(315, 110, 300, 50, 350, 50) //the head (left and a little of the top)
-path.bezierCurveTo(365, 50, 380, 120, 395, 120) //the head (right)
-path.bezierCurveTo(440, 170, 485, 170, 490, 200) //the right arm, top
+path.bezierCurveTo(325, 110, 300, 50, 350, 50) //the head (left and a little of the top)
+path.bezierCurveTo(375, 50, 370, 120, 395, 120) //the head (right)
+path.bezierCurveTo(445, 170, 485, 170, 490, 200) //the right arm, top
 path.bezierCurveTo(475, 220, 400, 150, 395, 190) //the bottom of the right arm
 path.bezierCurveTo(380, 250, 450, 395, 425, 400) //the right leg, right side
 path.bezierCurveTo(390, 390, 400, 305, 350, 260) //the right leg, left side
 path.bezierCurveTo(300, 240, 300, 390, 275, 400) //the left leg, right side
-path.bezierCurveTo(265, 390, 250, 300, 300, 190) //the left leg, left side
-//path.quadraticCurveTo(500, 220, 490, 240);
+path.bezierCurveTo(235, 390, 320, 220, 300, 180) //the left leg, left sideside
+path.bezierCurveTo(300, 150, 185, 245, 210, 190) //the left arm, bottom
+path.bezierCurveTo(250, 170, 285, 120, 299, 120) //the left arm, top
+//path.quadraticCurveTo(250, 170, 300, 120);
 
 path.closePath();
 
