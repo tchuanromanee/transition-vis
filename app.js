dcb1adae0905464b150f63b5f74b876c0cb68e43
Tya Chuanromanee <tchuanro@nd.edu>
2022-11-04 15:04:18 -0400

    Set up server and draw the initial transition line

--- app.js
+++ app.js
@@ -1,6 +1,111 @@
 var connect = require('connect');
- var serveStatic = require('serve-static');
+var serveStatic = require('serve-static');
+var express = require('express');
+var http = require("http");
+const fs = require('fs');
 
- connect()
-     .use(serveStatic(__dirname))
-     .listen(8080, () => console.log('Server running on 8080...'));
+
+var entriesArray;
+const newEntry = {
+  "entryID": 4,
+  "Type": "Body",
+  "Date": "Tue, 19 Aug 1975 23:15:30 GMT",
+  "EmotionScale": 5,
+  "EmotionColor": "#EEEEEE",
+  "BodyPositionX": 300,
+  "BodyPositionY": 100,
+  "TimelineID": 1,
+  "TimelinePositionX": 100,
+  "TimelinePositionY": 0,
+  "Title": "Top Surgery Consult",
+  "Caption": "Got a consult with Dr. Y. He seemed nice and was able to answer my questions.",
+  "ImgID": ""
+};
+
+function readEntries(_callback) {
+  console.log("read entries called");
+  // Write to the JSON File
+  var hasReadFile = false;
+  //Read the JSON file to get the entries
+  fs.readFile("./data/sampleData.json", "utf8", (err, jsonString) => {
+    if (err) {
+      console.log("File read failed:", err);
+      return;
+    }try {
+      entriesArray = JSON.parse(jsonString);
+      for (var i=0;i<entriesArray.length;i++) {
+           console.log(entriesArray[i].entryID);
+        }
+        entriesArray.push(newEntry);
+        //console.log(JSON.stringify(entriesArray))
+        hasReadFile = true;
+        console.log("calling write entries");
+        _callback(); //https://stackoverflow.com/questions/21518381/proper-way-to-wait-for-one-function-to-finish-before-continuing
+        } catch (err) {
+      console.log("Error parsing JSON string:", err);
+    }
+  });
+
+
+    /* THE FOLLOWING APPEND WORKS!!! But writing to the file doesn't :()
+    fs.appendFile('./data/sampleData.json', JSON.stringify(newEntry), function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });*/
+
+
+}
+
+function writeEntries() {
+  console.log("write entries called");
+  var newString = JSON.stringify(entriesArray) //undefined
+  console.log(typeof(newString)) //undefined
+
+  fs.writeFile("./data/test.json", newString, (err) => {
+  if (err)
+    console.log(err);
+  else {
+    console.log("File written successfully\n");
+    //console.log("The written has the following contents:");
+    //console.log(fs.readFileSync("books.txt", "utf8"));
+  }
+});
+}
+
+
+
+//Start up the server
+const host = 'localhost';
+const port = 8080;
+var path = "";
+let handleRequest = connect().use(express.static(__dirname + path));/*(request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};*/
+
+
+const server = http.createServer(handleRequest).listen(8080);
+
+
+//const server = http.createServer(requestListener);
+//server.listen(port, host, () => {
+//    console.log(`Server is running on http://${host}:${port}`);
+      //readEntries();
+//      readEntries(writeEntries);
+      //writeEntries();
+
+//});
+
+// connect()
+//     .use(serveStatic(__dirname))
+//     .listen(8080, () => console.log('Server running on 8080...'));
--- script/draw.js
+++ script/draw.js
@@ -4,23 +4,21 @@ var svgContainer = d3.select("#main").append("svg")
   	.attr("id", "#mainSVG");
 
 
-//Bezier Curve: P1, P2, Pfinal
-var path = d3.path();
-path.moveTo(300,120);
-path.bezierCurveTo(325, 110, 300, 50, 350, 50) //the head (left and a little of the top)
-path.bezierCurveTo(375, 50, 360, 120, 390, 119) //the head (right)
-path.bezierCurveTo(400, 119, 459, 180, 490, 200) //the right arm, top
-path.bezierCurveTo(495, 240, 400, 150, 395, 190) //the bottom of the right arm
-path.bezierCurveTo(380, 250, 450, 395, 425, 400) //the right leg, right side
-path.bezierCurveTo(390, 390, 400, 305, 350, 260) //the right leg, left side
-path.bezierCurveTo(300, 240, 300, 390, 275, 400) //the left leg, right side
-path.bezierCurveTo(235, 390, 320, 220, 300, 180) //the left leg, left sideside
-path.bezierCurveTo(300, 150, 185, 245, 210, 190) //the left arm, bottom
-path.bezierCurveTo(250, 170, 285, 120, 299, 120) //the left arm, top
-//path.quadraticCurveTo(250, 170, 300, 120);
-
-path.closePath();
-
+// Draw the body
+var bodyPath = d3.path();
+bodyPath.moveTo(300,120);
+bodyPath.bezierCurveTo(325, 110, 300, 50, 350, 50) //the head (left and a little of the top)
+bodyPath.bezierCurveTo(375, 50, 360, 120, 390, 119) //the head (right)
+bodyPath.bezierCurveTo(400, 119, 459, 180, 490, 200) //the right arm, top
+bodyPath.bezierCurveTo(495, 240, 400, 150, 395, 190) //the bottom of the right arm
+bodyPath.bezierCurveTo(380, 250, 450, 395, 425, 400) //the right leg, right side
+bodyPath.bezierCurveTo(390, 390, 400, 305, 350, 260) //the right leg, left side
+bodyPath.bezierCurveTo(300, 240, 300, 390, 275, 400) //the left leg, right side
+bodyPath.bezierCurveTo(235, 390, 320, 220, 300, 180) //the left leg, left sideside
+bodyPath.bezierCurveTo(300, 150, 185, 245, 210, 190) //the left arm, bottom
+bodyPath.bezierCurveTo(250, 170, 285, 120, 299, 120) //the left arm, top
+//bodyPath.quadraticCurveTo(250, 170, 300, 120);
+bodyPath.closePath();
 
 //var line = d3.line()
 //    .x((d) => xScale(d.x))
@@ -28,7 +26,24 @@ path.closePath();
     // curveBasis is used
 //    .curve(d3.curveBasisClosed);
 
+// Draw the initial TimelineID
+
+var timeline = d3.line();
+// Eventually, we will populate the timelinepoints with data taken from the entries
+var timelinePoints = [
+    [100, 20],
+    [150, 30],
+    [600, 20]
+];
+var timelinePath = timeline(timelinePoints);
+
 svgContainer.append("path")
-    .attr("d", path)
+    .attr("d", timelinePath)
+    .attr("stroke", "black")
+    .attr("fill", "none")
+    .attr("Stroke-width", "2");
+
+svgContainer.append("path")
+    .attr("d", bodyPath)
     .attr("fill", "steelblue")
     .attr("stroke", "green");
