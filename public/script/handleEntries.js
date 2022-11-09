93fe061965928feeedfde9884fcc37694a574b6d
Tya Chuanromanee <tchuanro@nd.edu>
2022-11-09 14:13:37 -0500

    Remove circle and line and entry info when entry is deleted

--- public/script/handleEntries.js
+++ public/script/handleEntries.js
@@ -23,31 +23,76 @@ function getEntryByID(entryIDtoFind) {
     }
   }
 }
+
+function getEntryIndexByID(entryIDtoFind) {
+  for (let i = 0; i < entriesArray.length; i++) {
+    if (entriesArray[i].entryID == entryIDtoFind) {
+      return i;
+    }
+  }
+}
+
+function deleteVisibleEntry() {
+  console.log("Deleting entry");
+  var indexToDelete = getEntryIndexByID(currentlyVisibleEntryID);
+  entriesArray.splice(indexToDelete, 1);
+  console.log(entriesArray);
+
+  //TODO: HIDE entry info and SEND the new entries array to server to WRITE CHANGES TO JSON
+  //Refresh to draw the circles and timeline again
+  deleteAllDotsAndTimeline();
+  drawDotsAndTimeline();
+  resetEntryView();
+}
 //parseEntries();
+var currentlyVisibleEntryID = -1;
 
 function displayEntry() {
   var idOfCircle = d3.select(this).attr('id').slice(-1); // Circle ID and entry ID are the same when we created the circle elements
   var thisEntry = getEntryByID(idOfCircle); //-1 for offsetting (ASSUMES THE ENTRY ID AND)
   console.log(thisEntry); // returns the circle that was clicked
-
+  currentlyVisibleEntryID = idOfCircle;
+  // Show the entry attributes
   $("#dateLabel").text("Date: ");
   $("#dateSpan").text(thisEntry.Date);
   $("#titleSpan").text(thisEntry.Title);
   $("#captionSpan").text(thisEntry.Caption);
 
-
+  // Show the buttons
   $("#editEntryButton").show();
   $("#deleteEntryButton").show();
 
   if (entriesArray[0].Type == "Body") {
     // Show the first dot
     document.getElementById("test1").style.display = "block";
-
   }
+}
+
+function resetEntryView() {
+  $("#dateLabel").text("");
+  $("#dateSpan").text("");
+  $("#titleSpan").text("");
+  $("#captionSpan").text("");
+
+  // Show the buttons
+  $("#editEntryButton").hide();
+  $("#deleteEntryButton").hide();
+
+  // hide the body dots
+  $(".bodyCircle").hide();
+
 }
 
 function addNewEntry(){
   console.log("Add new entry");
+}
+
+function deleteAllDotsAndTimeline() {
+
+  d3.selectAll(".timelineCircle").remove();
+  d3.selectAll(".timeline").remove();
+
+
 }
 
 function drawDotsAndTimeline() {
@@ -65,6 +110,7 @@ function drawDotsAndTimeline() {
       .attr('cy', yPos)
       .attr('id', circleID)
       .attr('r', 8)
+      .attr('class', "timelineCircle")
       .style('fill', emotionColor)
       .on("mouseover", function(d) {
           d3.select(this).style("fill", "#fff8ee");
@@ -85,6 +131,8 @@ function drawDotsAndTimeline() {
       .attr("d", timelinePath)
       .attr("stroke", "black")
       .attr("fill", "none")
+      .attr('class', "timeline")
+      .attr('id', "timeline")
       .attr("Stroke-width", "2");
 }
 
@@ -104,6 +152,7 @@ function drawBodyDots() {
       .attr('cy', yPos)
       .attr('r', 8)
       .attr('id', stringID)
+      .attr('class', "bodyCircle")
       .style('fill', emotionColor)
       .style('display', 'none');
   }
