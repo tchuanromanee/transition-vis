e21055722504fcf8389536d4a095bc0fe5c20fff
Tya Chuanromanee <tchuanro@nd.edu>
2022-12-05 10:13:59 -0500

    Make portname dyanmic for heroku deployment

--- server.js
+++ server.js
@@ -183,5 +183,5 @@ app.get('/about', function(req, res) {
 
 
 
-app.listen(8080);
-console.log('Server is listening on port 8080');
+app.listen(process.env.PORT || 8080);
+console.log('Server is listening...');
